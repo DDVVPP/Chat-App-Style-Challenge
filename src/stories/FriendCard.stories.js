913f590a1478ae3dbd281a8902777feaaeead42b
Darshin Van Parijs <darshinvp@gmail.com>
2020-05-14 16:32:43 -0400

    feat: hooked up friendCard story component, but text styles not working

---
+++ src/stories/FriendCard.stories.js
@@ -0,0 +1,29 @@
+import { action } from '@storybook/addon-actions';
+import { text, withKnobs, object } from '@storybook/addon-knobs';
+import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+import FriendCard from '../components/DirectMessaging/FriendCard';
+
+const userDetails = {
+  userId: 1,
+  firstName: 'Donald',
+  lastName: 'Johnson',
+  status: 'Online',
+  profilePhoto: require('../assets/users/Donald.png'),
+  messagesReceived: ['Placeholder', 'Analysis of foreign experience, as it is commo...'],
+  timeStamp: '3h ago',
+};
+
+export default {
+  title: 'Friend Card',
+  component: FriendCard,
+  decorators: [withKnobs],
+};
+
+export const Card = () => <FriendCard selectedFriend={object('userDetails', { ...userDetails })} />;
+
+Card.story = {
+  name: 'Friend Card',
+};
---
+++ src/stories/FriendListPanel.stories.js
@@ -0,0 +1,22 @@
+// import { action } from '@storybook/addon-actions';
+// import { text, withKnobs, boolean, number } from '@storybook/addon-knobs';
+// import { linkTo } from '@storybook/addon-links';
+// import { storiesOf } from '@storybook/react';
+// import React from 'react';
+
+// import FriendCard from '../components/DirectMessaging/FriendCard';
+// import FriendListPanel from '../components/DirectMessaging/FriendListPanel';
+
+// export default {
+//   title: 'Friend List Panel',
+//   component: FriendListPanel,
+//   decorators: [withKnobs],
+// };
+
+// export const FriendList = () => {
+//   return <FriendListPanel showApp={linkTo('Friend Card')} />;
+// };
+
+// FriendList.story = {
+//   name: 'Friend List Panel',
+// };
